a7870d9a6ad910f41d7c32c2d540f72f0574538c
louagsa <louagsa@W8-020449.iconos.be>
2015-11-27 16:22:58 +0100

    More refactoring

--- ClimateWatch/src/main/java/be/faros/ui/StartView.java
+++ ClimateWatch/src/main/java/be/faros/ui/StartView.java
@@ -32,49 +32,45 @@ import ua.net.freecode.chart.Marker;
 public class StartView extends VerticalLayout implements View {
 	private static final long serialVersionUID = 1L;
 	public static final String VIEW_NAME = "";
-	String[] hours = new String[] { "01", "03", "05", "07", "09", "11", "13", "15", "17", "19", "21", "23" };
+	String[] valuesXAxisTime; 
 	NativeSelect locationSelection;
 	InlineDateField calendar;
 	Chart chart;
 	AxisSystem axisSystem;
-	
+
 	@Autowired
 	ClimateWatchEventService eventService;
 
 	@PostConstruct
 	public void init() {
-		
-		locationSelection = makeLocationSelection();
-		calendar = makeCalendar();
-		chart = makeChartLayout();
-		
-		locationSelection.addValueChangeListener(e -> checkLocationNotNull(
-				locationSelection.getValue(), calendar.getValue()));
-		calendar.addValueChangeListener(e -> checkLocationNotNull(
-				locationSelection.getValue(), calendar.getValue()));
+
+		makeLocationSelection();
+		makeCalendar();
+		makeChartLayout();
+
+		locationSelection.addValueChangeListener(e -> checkLocationNotNull());
+		calendar.addValueChangeListener(e -> checkLocationNotNull());
 
 		addComponents(locationSelection, calendar, chart);
 	}
-	
-	private NativeSelect makeLocationSelection() {
-		NativeSelect locationSelection = new NativeSelect("Select an option", eventService.findAllLocations());
+
+	private void makeLocationSelection() {
+		locationSelection = new NativeSelect("Select an option", eventService.findAllLocations());
 		locationSelection.setNullSelectionAllowed(false);
 		locationSelection.setImmediate(true);
-		return locationSelection;
 	}
-	
-	private InlineDateField makeCalendar() {
-		InlineDateField calendar = new InlineDateField();
+
+	private void makeCalendar() {
+		calendar = new InlineDateField();
 		calendar.setValue(new Date());
 		calendar.setImmediate(true);
 		calendar.setTimeZone(TimeZone.getTimeZone("GMT 01:00"));
 		calendar.setLocale(Locale.ENGLISH);
 		calendar.setResolution(Resolution.DAY);
-		return calendar;
 	}
-	
-	private Chart makeChartLayout() {
-		Chart chart = new Chart();
+
+	private void makeChartLayout() {
+		chart = new Chart();
 		chart.addStyleName("UniqueColorsBlueGreenRedScheme");
 		chart.setWidth("100%");
 		chart.setHeight("400px");
@@ -83,41 +79,39 @@ public class StartView extends VerticalLayout implements View {
 		chart.setGeneralTitle("ClimateWatch");
 		chart.setLegendData(new String[] { "temperature" });
 		chart.setLegendItemWidth(112);
-		axisSystem = makeAxisSystem(chart);
-		axisSystem.setXDiscreteValues(hours);
-		return chart;
+		makeAxisSystem();
+		valuesXAxisTime = new String[] { "01", "03", "05", "07", "09", "11", "13", "15", "17", "19", "21", "23" };
+		axisSystem.setXDiscreteValues(valuesXAxisTime);
 	}
-	
-	private AxisSystem makeAxisSystem(Chart chart) {
-		AxisSystem axisSystem = chart.createAxisSystem(AxisHorizontal.Bottom, AxisVertical.Left);
+
+	private void makeAxisSystem() {
+		axisSystem = chart.createAxisSystem(AxisHorizontal.Bottom, AxisVertical.Left);
 		axisSystem.setVerticalAxisMaxValue(100);
 		axisSystem.setHorizontalAxisLabelAngle(45);
 		axisSystem.setHorizontalAxisTitle("time");
 		axisSystem.setVerticalAxisTitle("temperature");
 		axisSystem.setCurvePresentation(new CurvePresentation[] {
-				new CurvePresentation(new Marker(Marker.MarkerShape.Circle), 0, CurvePresentation.CurveKind.Line) 
-				});
-		return axisSystem;
+				new CurvePresentation(new Marker(Marker.MarkerShape.Circle), 0, CurvePresentation.CurveKind.Line) });
 	}
 
-	private void checkLocationNotNull(Object selectedLocation, Date selectedDate) {
-		if (selectedLocation != null) {
-			List<ClimateWatchEvent> events = eventService.findByDateAndLocation(selectedDate, ((Location) selectedLocation).getLocation_id());
-			addChartContent(events);
+	private void checkLocationNotNull() {
+		if (locationSelection.getValue() != null) {
+			addChartContent(eventService.findByDateAndLocation(calendar.getValue(),
+					((Location) locationSelection.getValue()).getLocation_id()));
 		}
 	}
 
 	private void addChartContent(List<ClimateWatchEvent> events) {
 
-		double[] array = new double[hours.length];
+		double[] valuesYAxisDegrees = new double[valuesXAxisTime.length];
 		for (ClimateWatchEvent ce : events) {
-			for (int i = 0; i < hours.length; i++) {
-				if (ce.getTime().get(Calendar.HOUR_OF_DAY) == Integer.parseInt(hours[i])) {
-					array[i] = (double) ce.getDegrees();
+			for (int i = 0; i < valuesXAxisTime.length; i++) {
+				if (ce.getTime().get(Calendar.HOUR_OF_DAY) == Integer.parseInt(valuesXAxisTime[i])) {
+					valuesYAxisDegrees[i] = (double) ce.getDegrees();
 				}
 			}
 		}
-		axisSystem.setYDiscreteValuesForAllSeries(new double[][] { array });
+		axisSystem.setYDiscreteValuesForAllSeries(new double[][] { valuesYAxisDegrees });
 	}
 
 	@Override
